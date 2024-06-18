1c604e01984a4116e18ee9539efb1745fb1d4bfd
all-coder <niranjanmagesh@gmail.com>
2024-06-18 18:30:43 +0530

    updated expense model

--- lib/models/expense_model.dart
+++ lib/models/expense_model.dart
@@ -1,11 +1,23 @@
+enum Tags {
+  food,
+  groceries,
+  utility,
+  entertainment,
+  transport,
+  health
+}
+
 class ExpenseModel {
-  const ExpenseModel(
-      {required this.name,
-      required this.amount,
-      required this.date,
-      required this.reciever});
+  const ExpenseModel({
+    required this.name,
+    required this.amount,
+    required this.date,
+    required this.reciever,
+    required this.tag
+  });
   final String name;
   final String amount;
   final DateTime date;
   final String reciever;
+  final Tags tag;
 }
