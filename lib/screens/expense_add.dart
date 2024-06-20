d17a81ec9dfb923cea6f12d6028aa89f16739c75
all-coder <niranjanmagesh@gmail.com>
2024-06-20 21:48:38 +0530

    added dropdown menu items

--- lib/screens/expense_add.dart
+++ lib/screens/expense_add.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:icons_plus/icons_plus.dart';
+import '../models/expense_model.dart';
 
 class ExpenseAdd extends StatefulWidget {
   const ExpenseAdd({super.key});
@@ -12,14 +13,15 @@ class ExpenseAdd extends StatefulWidget {
 
 class _ExpenseAddState extends State<ExpenseAdd> {
   Future<void> _getDate() async {
-    DateTime ? _pickedDate = await showDatePicker( //tells flutter the DateTime can either hold datetime or null value, that is
-    //why we used '?' operator.
+    DateTime? _pickedDate = await showDatePicker(
+      //tells flutter the DateTime can either hold datetime or null value, that is
+      //why we used '?' operator.
       context: context,
       initialDate: DateTime.now(),
       firstDate: DateTime(2000),
       lastDate: DateTime(2040),
     );
-    if(_pickedDate!=null){
+    if (_pickedDate != null) {
       setState(() {
         dateController.text = _pickedDate.toString().split(" ")[0];
       });
@@ -60,6 +62,7 @@ class _ExpenseAddState extends State<ExpenseAdd> {
           key: _formKey,
           child: Column(
             children: [
+              //nature of payment
               Container(
                 margin: const EdgeInsets.only(top: 10),
                 decoration: const BoxDecoration(
@@ -84,6 +87,8 @@ class _ExpenseAddState extends State<ExpenseAdd> {
               const SizedBox(
                 height: 10,
               ),
+
+              //amount of money paid
               Container(
                 margin: const EdgeInsets.only(top: 10),
                 decoration: const BoxDecoration(
@@ -100,7 +105,7 @@ class _ExpenseAddState extends State<ExpenseAdd> {
                       color: Colors.white,
                       size: 30,
                     ),
-                    contentPadding:EdgeInsets.only(top: 12),
+                    contentPadding: EdgeInsets.only(top: 12),
                     border: InputBorder.none,
                   ),
                 ),
@@ -108,6 +113,8 @@ class _ExpenseAddState extends State<ExpenseAdd> {
               const SizedBox(
                 height: 10,
               ),
+
+              //receiever of the payment
               Container(
                 margin: const EdgeInsets.only(top: 10),
                 decoration: const BoxDecoration(
@@ -132,13 +139,15 @@ class _ExpenseAddState extends State<ExpenseAdd> {
               const SizedBox(
                 height: 10,
               ),
+
+              //date picker
               Container(
                 margin: const EdgeInsets.only(top: 10),
                 decoration: const BoxDecoration(
                   color: Colors.red,
                 ),
                 child: TextFormField(
-                  onTap: (){
+                  onTap: () {
                     _getDate();
                   },
                   controller: dateController,
@@ -148,13 +157,41 @@ class _ExpenseAddState extends State<ExpenseAdd> {
                   ),
                   decoration: const InputDecoration(
                     prefixIcon: Icon(
-                        Icons.date_range_outlined,
+                      Icons.date_range_outlined,
                       color: Colors.white,
                       size: 30,
                     ),
                     contentPadding: EdgeInsets.only(top: 12),
                     border: InputBorder.none,
                   ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Container(
+                margin: const EdgeInsets.only(top: 10),
+                decoration: const BoxDecoration(
+                  color: Colors.red,
+                ),
+                child: DropdownButtonFormField(
+                  items: [
+                    //here we want to use map function to return a list tile with the
+                    //tag and a prefix icon.
+                    ...tagIcons.keys.map((item) {
+                      return DropdownMenuItem(
+                        child: ListTile(
+                          leading: Icon(tagIcons[item]),
+                          title: Text(
+                            item.name,
+                            style: GoogleFonts.jetBrainsMono(
+                              fontSize: 15,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                      );
+                    })
+                  ],
+                  onChanged: null,
                 ),
               ),
             ],
