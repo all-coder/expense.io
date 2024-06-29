6ac8253dc2d3094f06c65d6d0db5c895c2044de0
all-coder <niranjanmagesh@gmail.com>
2024-06-29 19:40:04 +0530

    made a few changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import './screens/home.dart';
 
-//############################LifeSYNC##################################
+//############################expense.io##################################
 
-//a personal expense/event , built for me(yeah i am building it for me)
-//personally aimed at writing cleaner code
-//unfortunately apps like this have been already built, i acknowledge it, 
-//yet i simply want to make my own, prefarebly use some ai tools
-
-
-
-void main() {
+Future<void> main() async {
+  await dotenv.load();
   runApp(const MyApp());
+
 }
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  // This widget is the root of the application.
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
